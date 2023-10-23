2c8ff06ee82645231ac6951aee296203cb36d3ee
Mario Espasa <mario_espasa@sime.es>
2023-10-23 13:26:31 +0200

    PokemonCard created

--- lib/ui/DetailPage/DetailPage.dart
+++ lib/ui/DetailPage/DetailPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex_test_flutter/data/models/Pokemon.dart';
+import 'package:swipeable_card_stack/swipeable_card_stack.dart';
 
 import '../../domain/bloc/pokemon/pokemon_bloc.dart';
 import '../PokedexPage/PokedexPage.dart';
@@ -16,15 +17,31 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+
+
   @override
   Widget build(BuildContext context) {
+    SwipeableCardSectionController _cardController = SwipeableCardSectionController();
+    late Pokemon _selectedPokemon;
+
+    initState(){
+      super.initState();
+      _selectedPokemon = widget.currentPokemon;
+
+    }
 
     return Material(
       child: Scaffold(
-        backgroundColor: typeColors[widget.currentPokemon.types[0]]?.withOpacity(0.4),
-        appBar: AppBar(title: Text(widget.currentPokemon.name)),
-        body: Hero(tag: "pokemonImage${widget.currentPokemon.id}", child: Image.network(widget.currentPokemon.imageUrl)),
-      ),
+          backgroundColor: typeColors[widget.currentPokemon.types[0]]?.withOpacity(0.4),
+          appBar: AppBar(title: Text(widget.currentPokemon.name)),
+          body: SwipeableCardsSection(
+            cardController: _cardController,
+            context: context,
+            items: [],
+          )
+
+          //Hero(tag: "pokemonImage${widget.currentPokemon.id}", child: Image.network(widget.currentPokemon.imageUrl)),
+          ),
     );
   }
 }
